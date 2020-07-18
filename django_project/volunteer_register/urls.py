4a47e410ce52ba44ac1ff20a831a119d8ec90f15
Grant King <grantryan.king@gmail.com>
2020-07-17 20:03:54 -0700

    Add views
    Add skeletons for new views and connect routes.

--- django_project/volunteer_register/urls.py
+++ django_project/volunteer_register/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
+    path('templates/', views.all_templates, name='all-templates'),
+    path('template/<int:template_id>/', views.template_detail, name='template-detail'),
+    path('submission/<int:submission_id>/', views.submission_detail, name='submission-detail'),
 ]
--- django_project/volunteer_register/views.py
+++ django_project/volunteer_register/views.py
@@ -10,4 +10,12 @@ def index(request):
 def register(request):
     return HttpResponse("Hello, volunteers. You're at the registration index.")
 
+def template_detail(request, template_id):
+    return HttpResponse(f"This is the detail view for template {template_id}.")
+
+def submission_detail(request, submission_id):
+    return HttpResponse(f"This is the detail view for application {submission_id}.")
+
+def all_templates(request):
+    return HttpResponse(f"This is the categorical view for all application templates.")
     
