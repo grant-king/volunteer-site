1e8efbe8d6f330e649ead37bf6874e7aef21d7bf
Grant King <grantryan.king@gmail.com>
2020-07-22 20:37:18 -0700

    Update views.py
    Flesh out data access operations for new views. Simple text layout now presents appropriate data using filtered querysets for template_detail, submission_detial, and all_templates.

--- django_project/volunteer_register/views.py
+++ django_project/volunteer_register/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
+from .models import ApplicationTemplate, ApplicationSubmission, Question, Answer
 
 def index(request):
     return HttpResponse("Hello, volunteers. You're at the home page.")
@@ -11,11 +9,20 @@ def register(request):
     return HttpResponse("Hello, volunteers. You're at the registration index.")
 
 def template_detail(request, template_id):
-    return HttpResponse(f"This is the detail view for template {template_id}.")
+    template = ApplicationTemplate.objects.get(pk=template_id)
+    questions = Question.objects.filter(application_template_id=template_id)
+    html_questions = "<br>".join([question.question_text for question in questions])
+    return HttpResponse(f"This is the detail view for the <b>{template}</b> template. Questions:<br>{html_questions}")
 
 def submission_detail(request, submission_id):
-    return HttpResponse(f"This is the detail view for application {submission_id}.")
+    submission = ApplicationSubmission.objects.get(pk=submission_id)
+    questions = Question.objects.filter(application_template_id=submission.application_template)
+    answers = Answer.objects.filter(application_submission_id=submission_id)
+    qa = zip(questions, answers)
+    html_qa = "<br>".join([f'{question.question_text}: {answer.answer_text}' for question, answer in qa])
+    return HttpResponse(f"This is the detail view for the application started on <b>{submission.start_date}</b>. Questions, answers:<br>{html_qa}")
 
 def all_templates(request):
-    return HttpResponse(f"This is the categorical view for all application templates.")
-    
+    template_list = ApplicationTemplate.objects.all()
+    html_template_list = "<br>".join([template.name for template in template_list])
+    return HttpResponse(f"This is the categorical view for all application templates:<br><b>{html_template_list}</b>")
