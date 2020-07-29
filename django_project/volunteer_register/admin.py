6c58bd8307ce725952debb97ef21d3bde33a0a58
Grant King <grantryan.king@gmail.com>
2020-07-29 15:41:30 -0700

    Add new models, model fields.
    Add Color and Category models. Add multiple fields to ApplicationTemplate model.
    
    Register models in admin.

--- django_project/volunteer_register/admin.py
+++ django_project/volunteer_register/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import ApplicationTemplate, ApplicationSubmission, Question, Choice, Answer
+from .models import ApplicationTemplate, ApplicationSubmission, Question, Choice, Answer, Color, CategoryTag
 
 admin.site.register(ApplicationTemplate)
 admin.site.register(ApplicationSubmission)
 admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(Choice)
+admin.site.register(Color)
+admin.site.register(CategoryTag)
--- django_project/volunteer_register/models.py
+++ django_project/volunteer_register/models.py
@@ -1,11 +1,33 @@
 from django.db import models
 
-class ApplicationTemplate(models.Model):
-    name = models.CharField(max_length=50)
-    organization = models.CharField(max_length=50)
+class Color(models.Model):
+    red = models.PositiveSmallIntegerField()
+    green = models.PositiveSmallIntegerField()
+    blue = models.PositiveSmallIntegerField()
 
     def __str__(self):
-        return self.name
+        return f'{self.red}, {self.green}, {self.blue}'
+
+
+class CategoryTag(models.Model):
+    label = models.CharField(max_length=18)
+
+    def __str__(self):
+        return f'{self.label}'
+
+
+class ApplicationTemplate(models.Model):
+    subject = models.CharField(max_length=50)
+    organization = models.CharField(max_length=50)
+    assignment = models.CharField(max_length=100)
+    image = models.ImageField(default='default.jpg', upload_to='application_pics')
+    background_color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    category_tags = models.ManyToManyField(CategoryTag)
+    accepted_registrations = models.IntegerField(default=0)
+    max_registrations = models.IntegerField(default=2)
+
+    def __str__(self):
+        return f'{self.subject}: {self.assignment}'
 
 
 class ApplicationSubmission(models.Model):
@@ -42,5 +64,4 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
 
