5b037eb0308cf9304571e5b37824bdd60692cb0a
Grant King <grantryan.king@gmail.com>
2020-08-05 21:34:46 -0700

    Update forms.py
    Update ApplicationTemplateForm ModelForm class to just use exclude keyword under meta to set the form fields that will be rendered within the Django page template. Remove manual form fields from this ModelForm.
    
    Add new AddQuestionForm Form class for capturing and creating new Question objects that will be related to a particular ApplicationTemplate by the associated view.

--- django_project/volunteer_register/forms.py
+++ django_project/volunteer_register/forms.py
@@ -1,16 +1,17 @@
 from django import forms
-from .models import ApplicationTemplate, ApplicationSubmission, Color, CategoryTag
+from .models import ApplicationTemplate, ApplicationSubmission, Color, CategoryTag, Question
 
 class ApplicationTemplateForm(forms.ModelForm):
-    subject = forms.CharField(label='Subject')
-    organization = forms.CharField(label='Your Organization')
-    assignment = forms.CharField(label='Assignment')
-    job_description = forms.CharField(label='Job Description', widget=forms.Textarea)
-    image = forms.ImageField(label='Listing Image')
-    background_color = forms.ModelChoiceField(queryset=Color.objects.all())
-    category_tags = forms.ModelMultipleChoiceField(queryset=CategoryTag.objects.all())
-    max_registrations = forms.IntegerField(label='Max Registrations')
 
     class Meta:
         model = ApplicationTemplate
-        fields = ['subject', 'organization', 'assignment', 'job_description', 'image', 'background_color', 'category_tags', 'max_registrations']
+        exclude = ['accepted_registrations']
+
+    
+class AddQuestionForm(forms.Form):
+
+    question1 = forms.CharField(widget=forms.Textarea)
+    question2 = forms.CharField(widget=forms.Textarea, required=False)
+    question3 = forms.CharField(widget=forms.Textarea, required=False)
+    
+
